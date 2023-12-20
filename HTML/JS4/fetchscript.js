ff30324c5f80a0383b7df12f9b353cf4f585b86b
spicepallu <121652015+spicepallu@users.noreply.github.com>
2023-12-20 12:20:15 +0530

    Add files via upload

---
+++ HTML/JS4/fetchscript.js
@@ -0,0 +1,65 @@
+// POst Data
+document.getElementById("btn").addEventListener("click", makerequest);
+
+// Promise Then 
+function makerequest() {
+ console.log("Button Clicked")
+ const myInit = {
+  method: 'POST',
+  headers: {
+   'Content-Type': 'application/json'
+  },
+  body: '{"name":"Pallavi", "job":"Domain Trainer"}'
+ }
+ fetch('https://reqres.in/api/users', myInit)
+  .then((res) => {
+   if (!res.ok) {
+    throw Error(res.statusText)
+   }
+   return res.json()
+  }).then((data) => {
+   console.log(data)
+  }).catch((error) => console.log(error))
+}
+
+// Async and Await
+// async function makerequest() {
+//  try {
+//   console.log("Button Clicked")
+//   const myInit = {
+//    method: 'POST',
+//    headers: {
+//     'Content-Type': 'application/json'
+//    },
+//    body: '{"name":"Rahul", "job":"Android App Dev"}'
+//   }
+//   const res = await fetch('https://reqres.in/api/users', myInit)
+//   if (!res.ok) {
+//    throw Error(res.statusText)
+//   }
+//   const data = await res.json()
+//   console.log(data)
+//  } catch (error) {
+//   console.log(error)
+//  }
+// }
+
+// function makerequest()
+// {
+//     console.log('Button Clicked')
+//     const myInit={
+// method:'POST',
+// headers:{'Content-Type':'application/json'},
+// body:{"name":"Pallavi","job":"Trainer"}
+
+//     }
+//     fetch('https://reqres.in/api/users',myInit)
+//     .then((res)=>
+//     {
+//         return res.json()
+//     }).then((data)=>
+//     {
+//         console.log(data)
+//     })
+// }
+
